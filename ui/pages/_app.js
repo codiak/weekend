40e553a9cca61e209c9824e7bf98d36f4f666692
Cody P <cody.pumper@buzzfeed.com>
2020-06-07 17:01:44 -0500

    Add Antd UI, display messages

---
+++ ui/pages/_app.js
@@ -0,0 +1,14 @@
+import App from 'next/app';
+import stylesheet from 'antd/dist/antd.min.css';
+
+
+class Application extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Component {...pageProps} />
+    );
+  }
+}
+
+export default Application;
--- ui/pages/index.js
+++ ui/pages/index.js
@@ -2,20 +2,36 @@ import React, { useState } from "react";
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import classNames from 'classnames'
+import { Input } from 'antd';
+
+const { Search } = Input;
+
 
 export default function Home() {
-  const [message, setMessage] = useState('');
+  const [draft, setDraft] = useState('');
+  const [messages, setMessages] = useState([]);
 
-  function sendMessage(text) {
-    fetch('/api/message', {
+  async function sendMessage(text) {
+    const res = await fetch('/api/message', {
       method: 'POST',
       body: JSON.stringify({message: text})
-    })
-    .then(res => {
-      console.log(res);
-    }, err => {
-      console.error(err);
     });
+    if (res.ok) {
+      let data = await res.json();
+      pushNewMessages([
+        {'from': 'user', 'text': text, 'date': Date.now()},
+        {'from': 'weekend', 'text': data.reply, 'date': Date.now()}
+      ]);
+    } else {
+      alert('Error sending message!');
+    }
+  }
+
+  function pushNewMessages(messageObj) {
+    let updatedMessages = messages.slice()
+    updatedMessages = updatedMessages.concat(messageObj);
+    setMessages(updatedMessages);
   }
 
   return (
@@ -29,10 +45,17 @@ export default function Home() {
         Thing!
       </div>
       <div className="contents">
-        <p>
-          <input type="text" onChange={e => setMessage(e.target.value)}/>
-          <button onClick={e => sendMessage(message)}>Send</button>
-        </p>
+        {messages.map(message => {
+          return (<div className={classNames('message', message.from === 'user' ? 'sent':'received')} key={message.date}>
+            { message.text }
+          </div>)
+        })}
+        <div className="inputWrap">
+          <Search placeholder="What's up?"
+            enterButton="Send"
+            size="large"
+            onSearch={value => sendMessage(value)}/>
+        </div>
       </div>
       <Footer />
 
@@ -55,13 +78,52 @@ export default function Home() {
         }
 
         .contents {
+          position: relative;
           padding: 2rem;
-          align-items: top;
           flex-grow: 1;
+        }
+
+        .inputWrap {
+          border-top: 1px solid #DEDEDE;
+          background: #fff;
+          padding: 20px 0;
+          width: calc(100% - 4rem); 
+          position: absolute;
+          bottom: 0;
+        }
+
+        input[type="text"] {
+          height: 2em;
+          line-height: px;
+        }
+
+        .input--btn {
+          width: calc(100% - 100px);
+        }
+
+        .btn--input {
+          height: 2em;
+          width: 100px;
         }
 
         p {
           margin-top: 0;
+        }
+
+        .message {
+          max-width: 75%;
+          background: #E8F9F2;
+          padding: 10px;
+          border-radius: 8px;
+          margin-bottom: 10px;
+          margin-right: auto;
+          color: #444444;
+        }
+
+        .message.sent {
+          margin-right: 0;
+          margin-left: auto;
+          background: #EAEAEA;
         }
       `}</style>
 
