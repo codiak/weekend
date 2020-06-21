b732d8f44e35b99800a2151b5e8a9e06da941b7c
Cody P <codypumper@gmail.com>
2020-06-20 20:05:56 -0500

    Server: lil cleaning

--- server/main.py
+++ server/main.py
@@ -15,16 +15,6 @@ class TextMessage(BaseModel):
     message: str
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.post('/message')
 async def message_post(message: TextMessage):
     if message:
