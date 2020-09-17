b98f1db2b396e3002812a0f49e8dc07f96f3dadd
Cody P <codypumper@gmail.com>
2020-09-16 20:44:10 -0500

    Manually define Auth0 redirectUri

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -3,18 +3,15 @@ import stylesheet from "antd/dist/antd.min.css";
 import "../style.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// TODO: determine if disabling "SSR"/pre-render is a better option
-if (typeof window === "undefined") {
-  var window = { location: { origin: 'wkend.work' } };
-}
 
 class Application extends App {
   render() {
     const { Component, pageProps } = this.props;
+    // TODO: dyanmically define redirectUri
     return  <Auth0Provider
         domain="dev-ak382c1x.auth0.com"
         clientId="G16Em7FW2l9jBA0B53X84n9w6eQIBfcV"
-        redirectUri={window.location.origin}
+        redirectUri="https://dev.wkend.work"
       >
       <Component {...pageProps} />
     </Auth0Provider>
